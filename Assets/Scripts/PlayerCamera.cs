6dc6cf3739d5d61a630e7df911c0f6b311a4619d
groobob <elitegem84@gmail.com>
2023-03-03 19:45:12 -0500

    Implemented Camera Movement

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    float rotationX;
+    float rotationY;
+
+    private void Start()
+    {
+        // locking your cursor and making it invisible
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        // Getting the mouse x and y input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.smoothDeltaTime * sensY;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.smoothDeltaTime * sensX;
+
+        // Setting rotation variables based on mouse input
+        rotationX -= mouseY;
+        rotationY += mouseX;
+
+        // Sets a rotation interval for the y rotation on the camera
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+
+        // Rotating the camera and player
+        transform.rotation = Quaternion.Euler(rotationX, rotationY, 0);
+    }
+}
